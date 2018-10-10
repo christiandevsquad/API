be9263f7e1dcbd07a521a1e84cbd3a47e2da8ed8
Christian <cristian@devsquad.com>
2018-10-10 12:53:31 -0300

    Created product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -2,14 +2,8 @@
 
 namespace App\Http\Resources\Product;
 
-/*  Original code
-    use Illuminate\Http\Resources\Json\JsonResource;
- */
 use Illuminate\Http\Resources\Json\JsonResource;
 
-/*  Original code 
-    class ProductResource extends JsonResource
- */
 class ProductResource extends JsonResource {
     /**
      * Transform the resource into an array.
@@ -23,8 +17,15 @@ class ProductResource extends JsonResource {
         'name' => $this->name,
         'description' => $this->detail,
         'price' => $this->price,
-        'stock' => $this->stock,
-        'discount' => $this->discount
+        'stock' => $this->stock > 0 ? 
+                   $this->stock : 'Out of stock',
+        'discount' => $this->discount,
+        'totalPrice' => round((1 - ($this->discount / 100)) * $this->price, 2),
+        'rating' => $this->reviews->count() > 0 ?
+                    round(($this->reviews->sum('star')) / ($this->reviews->count()), 2) : 'No rating yet',
+        'href' => [
+          'reviews'  => route('reviews.index', $this->id)
+        ]
       ];
     }
 }
