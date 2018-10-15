7e0cb3d18f44942a137dfe45ee82236a4a426b9b
Christian <cristian@devsquad.com>
2018-10-15 14:18:02 -0300

    Handles the exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,49 +3,66 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
+  /**
+   * A list of the exception types that are not reported.
+   *
+   * @var array
+   */
+  protected $dontReport = [
+    //
+  ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
+  /**
+   * A list of the inputs that are never flashed for validation exceptions.
+   *
+   * @var array
+   */
+  protected $dontFlash = [
+    'password',
+    'password_confirmation',
+  ];
 
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
+  /**
+   * Report or log an exception.
+   *
+   * @param  \Exception  $exception
+   * @return void
+   */
+  public function report(Exception $exception)
+  {
+    parent::report($exception);
+  }
+
+  /**
+   * Render an exception into an HTTP response.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Exception  $exception
+   * @return \Illuminate\Http\Response
+   */
+  public function render($request, Exception $exception)
+  {
+    if ($request->expectsJson()) {
+      if ($exception instanceof ModelNotFoundException) {
+        return response()->json([
+          'errors' => 'Product Model not found'
+        ], Response::HTTP_NOT_FOUND);
+      }
+
+      if ($exception instanceof NotFoundHttpException) {
+        return response()->json([
+          'errors' => 'Incorrect routes'
+        ], Response::HTTP_NOT_FOUND);
+      }
     }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
+    # dd($exception);
+    return parent::render($request, $exception);
+  }
 }
