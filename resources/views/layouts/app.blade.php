4467922d11985dda5780e86a09dc8e3eb350eb5a
Christian <cristian@devsquad.com>
2018-10-19 15:50:58 -0300

    Completed product managment page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,111 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<html lang="en">
+  <head>
+    <title>Vintage product stock</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+    .fakeimg {
+    height: 200px;
+    background: #aaa;
+    }
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    .sidenav{
+      font-family: "Courier New", Courier, monospace;
+      width: 100px;
+    }
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+		.navbar {
+				-webkit-box-shadow: 0 8px 6px -6px #999;
+				-moz-box-shadow: 0 8px 6px -6px #999;
+				box-shadow: 0 8px 6px -6px #999;
+		}
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    th{
+      color: #808080;
+    }
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+      width: 200px;
+    }
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    li a {
+      display: block;
+      color: #124;
+      padding: 8px 16px;
+      text-decoration: none;
+    }
 
-                    </ul>
+    /* Change the link color on hover */
+    li a:hover {
+      background-color: #555;
+      color: white;
+    }
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+    </style>
+    @yield('header')
+  </head>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+  <body>
+    <div class="container" style="margin-top:100px">
+      <nav class="navbar navbar-expand-sm navbar-light justify-content-end">
+        <a class="navbar-brand" href="#">
+          <img src="images/vintage.png" alt="">
+        </a>
+        <a class="navbar-brand ml-auto" href="#">
+          <img src="images/avatar.png" height="30" alt="logo">
+        </a>
+        <button type="button" class="btn btn-primary-outline" style="color: #000; background-color:white">SIGN OUT</button>
+      </nav>
+      @yield('navbar')
     </div>
-</body>
+
+    <div class="container" style="background-color: #f2f2f2;">
+      <div class="row" style="padding-top:30px;">
+        <div class="col-2">
+          <ul class="nav nav-pills flex-column sidenav">
+            <h4 style="text-align:center">MENU</h4>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PRODUCTS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ORDERS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CUSTOMERS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ANALITYCS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">DISCOUNTS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">APPS</a>
+            </li>
+          </ul>
+          <hr class="d-sm-none">
+        </div>
+
+        <div class="col-10">
+          @yield('right-side')
+        </div>
+      </div>
+    </div>
+
+    <footer class="page-footer font-small blue">
+      <div class="footer-copyright text-center py-3">
+        2018 Vintage - All rights reserved.
+      </div>
+      @yield('footer')
+    </footer>
+  </body>
 </html>
